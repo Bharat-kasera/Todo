1d3855ce38d816b44a59054e9fbe583179f21147
Bharat Kasera <bharat.codewolf@gmail.com>
2023-06-18 16:29:34 +0530

    done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,75 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+import React, { useState } from 'react';
+import './App.css';
+import './style.css';
+import CancelIcon from '@mui/icons-material/Cancel';
+
+const App = () => {
+  const [todos, setTodos] = useState([]);
+  const [searchTerm, setSearchTerm] = useState('');
+
+  const addTodo = (e) => {
+    e.preventDefault();
+    const todo = e.target.add.value.trim();
+    if (todo.length) {
+      setTodos([...todos, todo]);
+    }
+    e.target.reset();
+  };
+
+  const deleteTodo = (index) => {
+    setTodos(todos.filter((_, i) => i !== index));
+  };
+
+  const handleSearch = (e) => {
+    setSearchTerm(e.target.value.trim());
+  };
+
+  const filteredTodos = todos.filter((todo) =>
+    todo.toLowerCase().includes(searchTerm.toLowerCase())
   );
-}
+
+  return (
+    <section className="section-todo">
+      <div className="container">
+        <h1>TODO APP</h1>
+
+        <form className="search">
+          <input
+            className="form-control colorclass2"
+            type="text"
+            name="search"
+            placeholder="Search Todo... "
+            onChange={handleSearch}
+          />
+        </form>
+
+        <ul className="todos">
+          {filteredTodos.map((todo, index) => (
+            <li className="list-group-item" key={index}>
+              <span className="list-text">{todo}</span>
+              <span className="delete" onClick={() => deleteTodo(index)}>
+                <CancelIcon />
+              </span>
+            </li>
+          ))}
+        </ul>
+
+        <form className="add" onSubmit={addTodo}>
+          <label style={{ fontSize: '18px' }}>
+            Add new list
+          </label>
+          <input
+            className="form-control  colorclass"
+            type="text"
+            name="add"
+            placeholder="add todos... "
+          />
+        </form>
+      </div>
+    </section>
+  );
+};
 
 export default App;
